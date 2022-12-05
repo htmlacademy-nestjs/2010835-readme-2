dafae98267d4a651a813019118b0a129c8e15526
BSI <bsi_acs@mail.ru>
2022-12-05 19:52:02 +0300

    Подготовлен .env-файл для подключения к MongoDB. Реализована валидация

--- readme/apps/users/src/app/app.module.ts
+++ readme/apps/users/src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { ConfigModule } from '@nestjs/config';
+import { ENV_FILE_PATH } from './app.constants';
+import databaseConfig from '../config/database.config';
+import envSchema from './env.schema';
 
 @Module({
-  imports: [BlogUserModule, AuthenticationModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig],
+      validationSchema: envSchema,
+    }),
+    BlogUserModule,
+    AuthenticationModule],
   controllers: [],
   providers: [],
 })
